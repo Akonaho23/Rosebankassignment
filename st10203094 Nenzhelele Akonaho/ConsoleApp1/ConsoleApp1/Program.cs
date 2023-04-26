d08eff06c4b9799a94851130bc9ba84dc1949f13
Akonaho23 <bonotshilamu@gmail.com>
2023-04-26 18:23:32 +0200

    at the moment our system is asking the user how many ingridients they want and we populate the menu for the user

--- st10203094 Nenzhelele Akonaho/ConsoleApp1/ConsoleApp1/Program.cs
+++ st10203094 Nenzhelele Akonaho/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+using ConsoleApp1.NewFolder;
+
+
+
+
+
+
+
+
+Console.WriteLine("How many ingridients for the food ?");
+int numIngridients = Convert.ToInt32(Console.ReadLine());
+
+Recipe recipe = new Recipe(numIngridients);
+
+
+Console.WriteLine("Menu \n1-Add recipe\n2-Display recipe\n3-Update Quantity\n4-Update measurement\n5-Reset" +
+    " Quatnity\nClear recipe\n0-Exit");
+int option = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
+
