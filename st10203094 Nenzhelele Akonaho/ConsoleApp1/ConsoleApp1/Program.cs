3bdd005ffccad9865f9047af395d30162dd4426f
Akonaho23 <bonotshilamu@gmail.com>
2023-04-26 18:30:39 +0200

    now user can enter ingridients, update ingridients and using the menu

--- st10203094 Nenzhelele Akonaho/ConsoleApp1/ConsoleApp1/Program.cs
+++ st10203094 Nenzhelele Akonaho/ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,11 +14,67 @@ int numIngridients = Convert.ToInt32(Console.ReadLine());
 
 Recipe recipe = new Recipe(numIngridients);
 
-
+recipe.enterIngridients();
 Console.WriteLine("Menu \n1-Add recipe\n2-Display recipe\n3-Update Quantity\n4-Update measurement\n5-Reset" +
     " Quatnity\nClear recipe\n0-Exit");
 int option = Convert.ToInt32(Console.ReadLine());
 
+
+while (option != 0) {
+
+    Console.WriteLine("###########################################################################################");
+
+    switch (option)
+    {
+
+        case 1:
+
+            recipe.enterIngridients();
+            break;
+        case 2:
+            if (recipe.getNumIngrdient() > 0)
+            {
+                recipe.displayIngridients();
+            }
+            else { Console.WriteLine("Could not display ingridients and descriptions because there are no detail"); }
+            break;
+        case 3:
+            if (recipe.getNumIngrdient() > 0)
+            {
+                recipe.updateQuantity();
+            }
+            else { Console.WriteLine("Could not update quantity because there are no detail"); }
+            break;
+        case 4:
+            if (recipe.getNumIngrdient() > 0)
+            {
+                recipe.resetQuantities();
+            }
+            else { Console.WriteLine("Could not reset quantity(s) because there are no details"); }
+            break;
+        case 5:
+            if (recipe.getNumIngrdient() > 0)
+            {
+                recipe.clearRecipe();
+            }
+            else { Console.WriteLine("Could not clear recipe because there no ingridients");
+                Console.WriteLine("###########################################################################################");
+            }
+            break;
+        case 0:
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Please enter a value from 0-5");
+            break;
+
+        
+    }
+    
+    Console.WriteLine("Menu \n1-Add recipe\n2-Display recipe\n3-Update Quantity\n4-Reset\n5-Clear recipe" +
+   " Quatnity\nClear recipe\n0-Exit");
+    option = Convert.ToInt32(Console.ReadLine());
+}
 
 
 
