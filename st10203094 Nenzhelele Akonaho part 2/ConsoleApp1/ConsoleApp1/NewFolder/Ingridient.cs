da8e8a8076fe62cb337514ec8c59a306d00e7b44
Akonaho23 <bonotshilamu@gmail.com>
2023-05-27 18:30:00 +0200

    i added part 2

---
+++ st10203094 Nenzhelele Akonaho part 2/ConsoleApp1/ConsoleApp1/NewFolder/Ingridient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.NewFolder
+{
+    internal class Ingridient
+    {
+        decimal measurement;
+        String Name;
+        int originalQuantity;
+        int Quantity;
+        decimal numCalories;
+        String foodGroup;
+
+
+
+        public Ingridient(String name, int Quantity,decimal measurement, decimal numCalories, String foodGroup ) { 
+            this.Name = name;
+            this.Quantity = Quantity; 
+            this.measurement = measurement;
+            this.originalQuantity= Quantity;
+            this.numCalories = numCalories; 
+            this.foodGroup = foodGroup; 
+            
+        }
+
+        public String getName { get { return this.Name; } }
+        public int getQuantity { get { return this.Quantity; } }
+        
+        public decimal getMeasurement() { return this.measurement; }    
+
+        public decimal getnumCalories { get { return this.numCalories; } }
+        public String getfoodGroup { get { return this.foodGroup; } }
+        public int getOriginalQuantity { get { return this.originalQuantity; } }
+        public void setQuantity(int quantity) { this.Quantity = quantity;}
+
+    }
+}
---
+++ st10203094 Nenzhelele Akonaho part 2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,180 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+using ConsoleApp1.NewFolder;
+
+
+bool recipeMenu = true;
+int mainOption = 0;
+List<Recipe> recipes;
+
+List<Recipe> enterRecipes( int size) { 
+
+    for(int i = 0;i < size; i++) {
+        Console.WriteLine("What is the name of recipe " + (i+1) + "? ");
+        String recipeName = Convert.ToString(Console.ReadLine()) ?? "";
+
+        Console.WriteLine("How many ingridients of recipe " + recipeName + "? ");
+        int numIngridients = Convert.ToInt32(Console.ReadLine());
+
+        Recipe obRecip = new Recipe(numIngridients, recipeName);
+
+        
+
+        obRecip.enterIngridients();
+
+        recipes.Add(obRecip);
+    }
+
+    List<Recipe> recipeslist =  recipes.OrderBy(o=>o.GetRecipeName()).ToList();
+
+
+    return recipeslist;
+}
+
+
+void displayRecipe(int index) {
+
+    int option = -1;
+
+    while (option != 0)
+    {
+
+        Console.WriteLine("###########################################################################################");
+
+        recipes.ElementAt(index).displayIngridients();
+
+        switch (option)
+        {
+
+            case 1:
+
+                recipes.ElementAt(index).enterIngridients();
+                break;
+            case 2:
+                if (recipes.ElementAt(index).getNumIngrdient() > 0)
+                {
+                    recipes.ElementAt(index).displayIngridients();
+                }
+                else { Console.WriteLine("Could not display ingridients and descriptions because there are no detail"); }
+                
+                break;
+            case 3:
+                if (recipes.ElementAt(index).getNumIngrdient() > 0)
+                {
+                    recipes.ElementAt(index).updateQuantity();
+                }
+                else { Console.WriteLine("Could not update quantity because there are no detail"); }
+                
+                break;
+            case 4:
+                if (recipes.ElementAt(index).getNumIngrdient() > 0)
+                {
+                    recipes.ElementAt(index).resetQuantities();
+                }
+                else { Console.WriteLine("Could not reset quantity(s) because there are no details"); }
+               
+                break;
+            case 5:
+                if (recipes.ElementAt(index).getNumIngrdient() > 0)
+                {
+                    recipes.ElementAt(index).clearRecipe();
+                }
+                else { Console.WriteLine("Could not clear recipe because there no ingridients");
+                    Console.WriteLine("###########################################################################################");
+                }
+                break;
+            case 6:
+                mainOption = -1;
+                return;
+                break;
+            case 0:
+                Environment.Exit(0);
+                break;
+            default:
+                Console.WriteLine("Please enter a value from 0-5");
+                break;
+
+
+        }
+        
+        Console.WriteLine("Menu \n1-Add recipe\n2-Display recipe\n3-Update Quantity\n4-Reset\n5-Clear recipe" +
+       " Quatnity\nClear \n6-Go to Main Menu recipe\n0-Exit");
+        option = Convert.ToInt32(Console.ReadLine());
+    }
+
+}
+
+void displayRecipes(List<Recipe> recipes) {
+    Console.Clear();    
+    Console.WriteLine("Recipe List:");
+    for (int i = 0; i<recipes.Count; i++)
+    {
+        
+        Console.WriteLine ((i+1)+ "." + recipes.ElementAt(i).GetRecipeName());
+    }
+    Console.WriteLine();
+
+
+}
+
+
+
+
+Console.WriteLine("How many recipes ?");
+int numRecipes = Convert.ToInt32(Console.ReadLine());
+
+int optionMainMenu = 0;
+
+ recipes = new List<Recipe>();
+
+recipes = enterRecipes(numRecipes);
+
+int option = -1;
+
+displayRecipes(recipes);
+
+Console.WriteLine("Enter the number of recipe you want to display or 0 to exit");
+
+int selectRecipe = Convert.ToInt32(Console.ReadLine());
+while (selectRecipe < 0 || selectRecipe > (numRecipes+1))
+{
+    Console.WriteLine("Please enter the value from " + 1 + " to " + numRecipes);
+    selectRecipe = Convert.ToInt32(Console.ReadLine());
+}
+Console.Clear();
+mainOption = -2;
+
+while (mainOption != 0)
+{
+
+    switch (mainOption)
+    {
+        case -1:
+            Console.Clear();
+            displayRecipes(recipes);
+            Console.WriteLine("Enter the number of recipe you want to display 0 to exit");
+            selectRecipe = Convert.ToInt32(Console.ReadLine());
+            while (selectRecipe < 0 || selectRecipe > (numRecipes+1))
+            {
+                Console.WriteLine("Please enter the value from " + 1 + " to " + numRecipes);
+                selectRecipe = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Clear();
+            mainOption = -2;
+            break;
+        case -2:
+            Console.Clear();
+            displayRecipe(selectRecipe-1);
+
+            break;
+
+    }
+}
+
+
+
+
+
+
+
+
